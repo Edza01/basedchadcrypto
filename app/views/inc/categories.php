4d91d6b37f563a8be209c690dc3f9891b849fbc9
Edgars01 <miclex82@tutanota.com>
2022-12-26 23:51:43 +0200

    categories

--- app/views/inc/categories.php
+++ app/views/inc/categories.php
@@ -3,7 +3,7 @@
         <div class='categories-content'>
             <i class='fa-solid fa-person-chalkboard fa-2xl'></i>
             <h3>Crypto Guides</h3>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, totam velit? Iure nemo labore inventore?</p>
+            <p>Learn about cryptocurrency and investing with our crypto guide.</p>
             <button class='categories-button' href='#'>Read More</button>
         </div>
     </div>
@@ -13,8 +13,8 @@
     <div class='categories-box'>
         <div class='categories-content'>
             <i class='fa-solid fa-wallet fa-2xl'></i>
-            <h3>Best Crypto wallets</h3>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, totam velit? Iure nemo labore inventore?</p>
+            <h3>Crypto wallets</h3>
+            <p>Learn about different types of crypto wallets.</p>
             <button class='categories-button' href='#'>Read More</button>
         </div>
     </div>
@@ -25,7 +25,7 @@
         <div class='categories-content'>
             <i class='fa-solid fa-chart-line fa-2xl'></i>
             <h3>Crypto Predictions</h3>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, totam velit? Iure nemo labore inventore?</p>
+            <p>Learn about crypto predictions for the future.</p>
             <button class='categories-button' href='#'>Read More</button>
         </div>
     </div>
@@ -35,8 +35,8 @@
     <div class='categories-box'>
         <div class='categories-content'>
             <i class='fa-solid fa-chart-line fa-2xl'></i>
-            <h3>Best Crypto Exchanges</h3>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, totam velit? Iure nemo labore inventore?</p>
+            <h3>Crypto Exchanges</h3>
+            <p>Platforms for buying, selling, and trading cryptocurrencies.</p>
             <button class='categories-button' href='#'>Read More</button>
         </div>
     </div>
@@ -46,8 +46,8 @@
     <div class='categories-box'>
         <div class='categories-content'>
             <i class='fa-solid fa-chart-line fa-2xl'></i>
-            <h3>Crypto Calculator</h3>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, totam velit? Iure nemo labore inventore?</p>
+            <h3>Crypto Profit Calculator</h3>
+            <p>Calculate cryptocurrency profit or loss.</p>
             <button class='categories-button' href='#'>Read More</button>
         </div>
     </div>
@@ -68,8 +68,8 @@
     <div class='categories-box'>
         <div class='categories-content'>
             <i class='fa-solid fa-chart-line fa-2xl'></i>
-            <h3>Crypto ATM Locations</h3>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, totam velit? Iure nemo labore inventore?</p>
+            <h3>Crypto ATMs</h3>
+            <p>Cryptocurrency ATMs allow users to buy and sell digital coins with cash or debit cards.</p>
             <button class='categories-button' href='#'>Read More</button>
         </div>
     </div>
