31bd59fdfdc5eec222163e57f1a1ddf07f133b8d
Edgars01 <miclex82@tutanota.com>
2022-12-27 00:04:31 +0200

    icons, text

--- app/views/inc/categories.php
+++ app/views/inc/categories.php
@@ -34,7 +34,7 @@
 <div class='categories-card'>
     <div class='categories-box'>
         <div class='categories-content'>
-            <i class='fa-solid fa-chart-line fa-2xl'></i>
+            <i class="fa-solid fa-right-left fa-2xl"></i>
             <h3>Crypto Exchanges</h3>
             <p>Platforms for buying, selling, and trading cryptocurrencies.</p>
             <button class='categories-button' href='#'>Read More</button>
@@ -45,7 +45,7 @@
 <div class='categories-card'>
     <div class='categories-box'>
         <div class='categories-content'>
-            <i class='fa-solid fa-chart-line fa-2xl'></i>
+            <i class="fa-solid fa-calculator fa-2xl"></i>
             <h3>Crypto Profit Calculator</h3>
             <p>Calculate cryptocurrency profit or loss.</p>
             <button class='categories-button' href='#'>Read More</button>
@@ -56,7 +56,7 @@
 <div class='categories-card'>
     <div class='categories-box'>
         <div class='categories-content'>
-            <i class='fa-solid fa-chart-line fa-2xl'></i>
+            <i class="fa-solid fa-person-circle-question fa-2xl"></i>
             <h3>Who Accepts Crypto as Payment?</h3>
             <p>Businesses and organizations that accept cryptocurrency as payment.</p>
             <button class='categories-button' href='#'>Read More</button>
@@ -67,7 +67,7 @@
 <div class='categories-card'>
     <div class='categories-box'>
         <div class='categories-content'>
-            <i class='fa-solid fa-chart-line fa-2xl'></i>
+            <i class="fa-solid fa-money-bills fa-2xl"></i>
             <h3>Crypto ATMs</h3>
             <p>Cryptocurrency ATMs allow users to buy and sell digital coins with cash or debit cards.</p>
             <button class='categories-button' href='#'>Read More</button>
--- app/views/pages/index.php
+++ app/views/pages/index.php
@@ -77,25 +77,23 @@
 
 
   <div class="jumbotron">
-    <h1 class="display-4">MyFreeGirlsClub</h1>
-    <p class="lead main-text"><b>Welcome to MyFreeGirlClub</b>, a place where you can indulge your love of all things feminine and beautiful.
-      Whether you're a fan of fashion, a lover of art, or simply enjoy admiring the female form, this website has something for you.
-      With a diverse selection of gorgeous gals from around the world, you'll be able to
-      feast your eyes on a wide range of looks, styles, and personalities.
-      From stunning fashion models to natural beauties, you'll find a variety of women to admire.
-      Each and every one of them is captured in breathtaking images that showcase their unique charms and appeal.
-      Whether you're drawn to bold and confident women or prefer a more delicate and demure aesthetic, you'll find something to suit your taste.
-      So don't wait any longer - dive into the world of MyFreeGirlClub and discover all the delights it has to offer.
-      With its playful and inviting atmosphere, you'll feel like you're in a paradise for those who love gorgeous gals.
-      So come on in and enjoy the company of these tantalizing beauties.
+    <h1 class="display-4">Informative crypto blog</h1>
+    <p class="lead main-text">Welcome to our informative crypto blog! Here you will find a wealth of 
+      information and resources on all things cryptocurrency. Whether you are new 
+      to the world of crypto or an experienced investor, we have something for you.
+      On our blog, you will find articles and guides on topics such as how to buy and sell 
+      cryptocurrencies, how to securely store your digital assets, and the latest news and developments 
+      in the world of crypto. We also offer in-depth reviews of different cryptocurrency exchanges and wallets, 
+      as well as analysis and commentary on the market and its trends.
+      In addition to our written content, we also offer a range of interactive resources, 
+      such as calculators, to help you learn more about crypto and make informed decisions about your investments.
+      So if you are looking for a one-stop-shop for all your crypto needs, look no further! Join our community 
+      and stay up to date with the latest in the world of cryptocurrency.
     </p>
     <hr class="my-4">
     <p class="main-text">
-      MyFreeGirlsClub is a playful paradise for those who love to gaze upon gorgeous gals from around the globe!
-      This tantalizing website features a diverse selection of beauties with a wide range of looks, styles,
-      and personalities, all captured in stunning images. Whether you're into fashion, beauty, or simply
-      adore the female form, this website is sure to have something that will captivate and inspire you. So why wait?
-      Head on over to MyFreeGirlClub and get your fix of fun and flirty females today!
+      Learn about cryptocurrency with our educational blog. Stay up to date with the latest news 
+      and trends and use our interactive resources to make informed investment decisions.
     </p>
   </div>
 
