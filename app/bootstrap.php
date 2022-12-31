15fb344af59f2f1c6e29435ce5f5b9a6af43b952
Edgars01 <miclex82@tutanota.com>
2023-01-01 01:37:15 +0200

    error helper

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -4,7 +4,7 @@
   // Load Helpers
   require_once 'helpers/url_helper.php';
   require_once 'helpers/session_helper.php';
-  require_once 'helpers/color-based-on-tag';
+  require_once 'helpers/color-based-on-tag.php';
   require_once 'helpers/time-ago.php';
 
 
